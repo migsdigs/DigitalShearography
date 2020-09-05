32521bf14e18efccfe438f529dc5babec824ef0e
Miguel Garcia Naude <miguelito2384@gmail.com>
2020-09-05 17:36:32 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+
+class DS_GUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+
+app = DS_GUI()
+app.mainloop()
